4f916634ad2e056ddc25b5383185fca38b91c0ee
TiagoZD <57190443+TiagoZD@users.noreply.github.com>
2019-12-11 13:34:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,20 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import ColumnList from "./ColumnList";
+import SearchBox from "./SearchBox";
 
-const ToDoList = () => {
-  const [task, setTask] = useState("");
-  const [list, setList] = useState([]);
+import { render } from "react-dom";
 
-  const addNewTask = () => {
-    if (task.trim() !== "") {
-      setList(oldArray => [...oldArray, task]);
-      setTask("");
-    }
-  };
-
-  const handleRemoveItem = todoIndex => {
-    const newTodos = list.filter((_, index) => index !== todoIndex);
-    setList(newTodos);
-  };
+const App = () => {
+  useEffect(() => {
+    localStorage.setItem("columns", JSON.stringify([]));
+    localStorage.setItem("cards", JSON.stringify([]));
+  });
 
   return (
-    <div className="App">
-      <div>
-        <h1>Esta es tu lista: </h1>
-      </div>
-      <div>
-        Agregar un elemento a la lista
-        <input
-          class="txtBox"
-          placeholder="Elemento"
-          type="text"
-          required="required"
-          rows="1"
-          value={task}
-          maxlength="30"
-          onChange={event => {
-            setTask(event.target.value);
-          }}
-        />
-        <button class="myButton" onClick={addNewTask}>
-          Agregar
-        </button>
-      </div>
-
-      <div>
-        {list.map((task, index) => (
-          <li key={index} className="li">
-            <div className="text">{task}</div>
-            <button
-              class="x"
-              onClick={() => {
-                handleRemoveItem(index);
-              }}
-            >
-              x
-            </button>
-          </li>
-        ))}
-      </div>
+    <div>
+      <ColumnList /> <SearchBox />
     </div>
   );
 };
 
-export default ToDoList;
+render(<App />, document.getElementById("root"));
